d63abae7228a79561b9ef9bc0feacf6a33e5c01a
amirhooo <amirwh@30mail.ir>
2016-04-01 21:10:46 +0430

    Create ver.lua

---
+++ plugins/ver.lua
@@ -0,0 +1,20 @@
+do
+
+function run(msg, matches)
+  return 'Pga Bot Beta ' [[ 
+  maked by:
+  amir wh
+  and
+  amir pga]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "!version: Shows bot version",
+  patterns = {
+    "^!version$"
+  }, 
+  run = run 
+}
+
+end
