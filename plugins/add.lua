6d2f3ea1f6dd48bcf55fdf61425f2cec0d8aeaa6
amirhooo <amirwh@30mail.ir>
2016-04-02 08:53:55 +0430

    Create add.lua

---
+++ plugins/add.lua
@@ -0,0 +1,9 @@
+if is_super_group(msg) then
+				return reply_msg(msg.id, 'SuperGroup is already added.', ok_cb, false)
+			end
+			print("SuperGroup "..msg.to.print_name.."("..msg.to.id..") added")
+			savelog(msg.to.id, name_log.." ["..msg.from.id.."] added SuperGroup")
+			superadd(msg)
+			set_mutes(msg.to.id)
+			channel_set_admin(receiver, 'user#id'..msg.from.id, ok_cb, false)
+		end
