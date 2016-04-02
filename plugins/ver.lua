6caef3de26006c11e8e00b9ce38e1391e856111a
amirhooo <amirwh@30mail.ir>
2016-04-02 08:46:45 +0430

    Update ver.lua

--- plugins/ver.lua
+++ plugins/ver.lua
@@ -1,18 +1,30 @@
 do
 
 function run(msg, matches)
-  return 'Pga Bot Beta ' [[ 
-  maked by:
+  return 'Pga Bot Beta '..version.. [[ 
+ مشخصات فنی سرور :
+12ساعت 
+1کاربره
+--------------
+  توسئه دهندگان :
   amir wh
   and
-  amir pga]]
+  amir pga
+---------------
+پلهای ارتباطی :
+@amir_pga
+site:coming son
+لینک ساپورت :coming son
+chanel:coming son
+
+  ]]
 end
 
 return {
   description = "Shows bot version", 
   usage = "!version: Shows bot version",
   patterns = {
-    "^!version$"
+    "^!ver$"
   }, 
   run = run 
 }
